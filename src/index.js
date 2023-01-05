060ecf0e1afd66f22738843254650aea5fe06d6f
alexveryk <veryk.av@gmail.com>
2023-01-05 22:43:33 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,74 +1,97 @@
+import fetchCountries from './fetchCountries';
+import Notiflix from 'notiflix';
 import './css/styles.css';
 let _ = require('lodash');
 
 const DEBOUNCE_DELAY = 300;
-const BASE_URL = 'https://restcountries.com/v3.1/name/';
 
 const searchBox = document.querySelector('#search-box');
 const countryInfo = document.querySelector('.country-info');
+const countryList = document.querySelector('.country-list');
 
 searchBox.addEventListener('input', _.debounce(onInput, 300));
 
-function fetchCountries(name) {
-  return fetch(`${BASE_URL}${name}`).then(resp => {
-    if (!resp.ok) {
-      console.log(resp);
-      console.log('Oops, there is no country with that name');
-      throw new Error(resp.statusText);
-    }
-    console.log(resp);
-    return resp.json();
-  });
-}
+// Винети її в окремий файл fetchCountries.js і зробити іменований експорт.
+
+//---------------------------------------------------
 
 function onInput(evt) {
-  fetchCountries(evt.target.value)
-    .then(res => {
-      if (res.length < 2) {
-        createOneMarkup(res);
-      } else if (res.length > 2 && res.length <= 10) {
-        createSeveralMarkup(res);
-      }
-    })
-    .catch(err => {
-      console.log(err);
-    });
+  let name = evt.target.value.trim();
+  if (name) {
+    fetchCountries(name)
+      .then(data => {
+        if (data.length < 2) {
+          createOneMarkup(data);
+        } else if (data.length > 2 && data.length <= 10) {
+          createSeveralMarkup(data);
+        } else {
+          countryList.innerHTML = '';
+          countryInfo.innerHTML = '';
+          console.log(
+            'Too many matches found. Please enter a more specific name.'
+          );
+          Notiflix.Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+        }
+      })
+      .catch(err => {
+        countryList.innerHTML = '';
+        countryInfo.innerHTML = '';
+        console.log(err);
+      });
+  }
 }
 
 function createSeveralMarkup(arr) {
+  countryInfo.innerHTML = '';
   const markup = arr
     .map(
-      ({ name: { common: countryName }, flags: { svg: flag } }) => `
-  <div class ="container">
-      <img src="${flag}" alt="" width ="48" height="32">
-      <h1>${countryName}</h1>
-          
-  </div>`
+      ({ capital, flags: { svg } }) => `
+      <li class="item">
+        <img src="${svg}" alt="${capital}"  width="48" height="32">
+        <h1>${capital}</h1>
+      </li>
+  `
     )
     .join('');
   console.log(markup);
-  countryInfo.insertAdjacentHTML('beforeend', markup);
+
+  countryList.innerHTML = markup;
 }
 
 function createOneMarkup(arr) {
+  countryList.innerHTML = '';
   const markup = arr
     .map(
       ({
-        name: { official: countryName },
-        flags: { svg: flag },
         capital,
+        flags: { svg },
+        name: { official },
         population,
         languages,
       }) => `
       <div class="container">
-      <img src="${flag}" alt="" width="48" height="32" />
-      <h1>${countryName}</h1>
-      <div class="container"><h2>Capital: ${capital}</h2></div>
-      <div class="container"><span>Population: </span>${population}</div>
-      <div class="container"><span>Languages </span>${languages}</div>
-    </div>`
+      <div class="wrap">
+        <img src="${svg}" alt="" width="48" height="32" />
+        <h1>${official}</h1>
+      </div>
+      <div class="wrap">
+        <span class="header">Capital:</span>
+        <span class="header-value">${capital}</span>
+      </div>
+      <div class="wrap"><span class="header">Population: </span><span class="header-value">${population}</span></div>
+      <div class="wrap">
+        <span class="header">Languages: </span><span class="header-value">${Object.values(
+          languages
+        )}&nbsp</span>
+      </div>
+    </div>
+      `
     )
     .join('');
+
   console.log(markup);
-  countryInfo.insertAdjacentHTML('beforeend', markup);
+
+  countryInfo.innerHTML = markup;
 }
